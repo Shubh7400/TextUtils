13aaf05324cfd01150e6cc23b42ff4fd3e21d2a9
shubh7400 <shubham.solanki7400@gmail.com>
2022-11-18 18:40:30 +0530

    updateing code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
 import './App.css';
+import TextInputs from './components/TextInputs';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+     <Navbar title="NavBar" aboutText="About us"/>
+     <About/>
+   <TextInputs 
+   heading ='Enter Text Here To Convertion'
+   />
+   
+   </>
   );
 }
 
---
+++ src/components/TextInputs.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react'
+
+const TextInputs = (props) => {
+  const[text,setText]=useState("enter text here")
+  const[edit,setEdit]=useState('')
+
+
+  const handleUpClick=()=>{
+    console.log("uppercase was clicked",text)
+    let newText=text.toUpperCase();
+    setEdit(newText)
+  }
+  const handleLoClick = () => {
+     console.log('lower')
+     let loText=text.toLowerCase();
+     setEdit(loText)
+   }
+
+
+   const handleOnChange = (event) => {
+     console.log("on change")
+     setText(event.target.value)
+    
+
+     }
+
+    const removeAll = () => {
+      alert("are you sure?")
+      setEdit([]);
+     
+ }
+  return (
+<>
+        <div className='container my-3 text-white'>
+          <div className='first'>
+         <h1>{props.heading}</h1>
+         <div className="mb-3">
+           <textarea className="form-control"   onChange={(event) => setText(event.target.value) }   id="myBox" rows="8"></textarea>
+         </div></div>
+         <div className='second'>
+         <button className='btn btn-success me-3' onClick={handleUpClick}>Convert to UpperCase</button>
+         <button className="btn btn-success me-3" onClick={handleLoClick}>Convert to Lowercase</button>
+         <button className="btn btn-success" onClick={removeAll}>Remove all</button>
+         </div>
+       </div>
+
+       <div className='container my-3'>
+        <div className='third'>
+     <textarea className='form-control' value={edit} onChange={handleOnChange}  id="myBox" rows="8"></textarea> 
+     </div></div>
+       <div className="container my-3">
+       <div className='four'>
+        <h1>Your Text summary</h1>
+        <b>{text.split(" ").length} Words And {text.length} Characters</b><br/>
+        <b>{0.008* text.split(" ").length} Minutes Read</b>
+        <h2>Preview</h2>
+        <b>{edit}</b>
+        </div>
+       </div>
+  </>     
+      
+  )
+}
+
+export default TextInputs
